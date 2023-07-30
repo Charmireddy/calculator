fe85e24dedc5534e4bd3258afabee9dc2fcf91ef
Ch.Charmi Reddy <85749261+Charmireddy@users.noreply.github.com>
2023-07-30 23:55:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Calci from './Components/Calci';
+
+function App() {
+  return (
+  
+    <div className="App">
+   
+     <Calci />
+    </div>
+  );
+}
+
+export default App;
